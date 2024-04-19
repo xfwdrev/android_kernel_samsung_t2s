1ca423b3b981ea160a45d871936fc982cc61b5ad
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2024-04-19 20:06:44 +0300

    drivers/gpu/arm: remove reliance on SEC variable

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 export ARCH=arm64
-export PLATFORM_VERSION=11
-export ANDROID_MAJOR_VERSION=r
-export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
 export LLVM=1
 
 make ARCH=arm64 LLVM=1 exynos2100-t2sxxx_defconfig
